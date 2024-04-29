9edcc908b45ecbc7f001bf6a96fa5725b1ebdcb6
gayomii <gayeon4041@gmail.com>
2024-04-29 20:13:12 +0900

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: true,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'auto',
+  printWidth: 100,
+};
---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>React Native Test</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
