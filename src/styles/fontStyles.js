fdc1b10ddc591ae7d89e7e34bb3c92d43b358b94
shhan <gkstjgms34@naver.com>
2024-05-02 15:58:52 +0900

    style: font & color 목록 수정, dummyData의 Date 랜덤 수정

--- src/styles/fontStyles.js
+++ src/styles/fontStyles.js
@@ -1,25 +1,36 @@
 import { StyleSheet } from 'react-native';
+import color from './colorPalette';
 
 const fontStyles = StyleSheet.create({
   basicFont: {
     fontFamily: 'Pretendard-Regular',
     fontSize: 16,
+    color: color.TEXT_PRIMARY,
+    letterSpacing: -0.32,
   },
   basicFont02: {
     fontFamily: 'Pretendard-Regular',
     fontSize: 14,
+    color: color.TEXT_PRIMARY,
+    letterSpacing: -0.28,
   },
   title01: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard-SemiBold',
     fontSize: 28,
+    color: color.TEXT_PRIMARY,
+    letterSpacing: -0.56,
   },
   title02: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard-SemiBold',
     fontSize: 20,
+    color: color.TEXT_PRIMARY,
+    letterSpacing: -0.4,
   },
   title03: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard-SemiBold',
     fontSize: 18,
+    color: color.TEXT_PRIMARY,
+    letterSpacing: -0.36,
   },
 });
 
