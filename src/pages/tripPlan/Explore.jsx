1a7d0f3de0090e08aafe6c58c94846d63a1a9df5
yein <yein@yeinui-MacBookPro.local>
2024-05-02 23:53:57 +0900

    design: 탐색 화면 구현

--- src/pages/tripPlan/Explore.jsx
+++ src/pages/tripPlan/Explore.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+import PlanList from './ExploreComponents/PlanList';
+import DiaryList from './ExploreComponents/DiaryList';
 
 const Explore = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
-      <Text>탐색</Text>
+      <View style={styles.categoryArea1}>
+        <View style={styles.categoryTextArea}>
+          <Text style={styles.categoryTitle}>실시간 인기 계획</Text>
+          <Text style={styles.moreViewText}>더보기..</Text>
+        </View>
+        <View style={styles.planListContiner}>
+          <PlanList />
+          <PlanList />
+          <PlanList />
+        </View>
+      </View>
+
+      <View style={styles.categoryArea2}>
+        <View style={styles.categoryTextArea}>
+          <Text style={styles.categoryTitle}>실시간 인기 기록</Text>
+          <Text style={styles.moreViewText}>더보기..</Text>
+        </View>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={styles.diaryListContiner}>
+          <DiaryList />
+          <DiaryList />
+          <DiaryList />
+          <DiaryList />
+          <DiaryList />
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
@@ -14,6 +43,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFF',
   },
+  categoryArea1: {
+    flex: 1,
+    padding: 20,
+    marginBottom: 30,
+  },
+  categoryArea2: {
+    padding: 20,
+    flex: 1,
+  },
+  categoryTextArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    marginVertical: 16,
+    alignItems: 'center',
+  },
+  categoryTitle: {
+    fontSize: 20,
+    letterSpacing: -0.4,
+    fontWeight: '500',
+  },
+  moreViewText: {
+    fontSize: 14,
+    letterSpacing: -0.28,
+    color: '#707070',
+  },
+  planListContiner: {
+    flex: 1,
+    alignItems: 'center', // 수평 가운데 정렬
+  },
+  diaryListContiner: {
+    flexDirection: 'row',
+  },
 });
 
 export default Explore;
+
---
+++ src/pages/tripPlan/ExploreComponents/DiaryList.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
+import HeartIcon from '../ExploreIcons/heart.png';
+import HeartIconFilled from '../ExploreIcons/heartfilled.png';
+import BookmarkIcon from '../ExploreIcons/bookmark.png';
+import BookmarkIconFilled from '../ExploreIcons/bookmarkfilled.png';
+import DiaryImage from '../ExploreIcons/diaryimage.png';
+
+const DiaryList = () => {
+  const [heartSelected, setHeartSelected] = useState(false);
+  const [bookmarkSelected, setBookmarkSelected] = useState(false);
+
+  return (
+    <View style={styles.diaryListBox}>
+      <Image source={DiaryImage} style={styles.imageExample} />
+      <View style={styles.infoContainer}>
+        <Text style={styles.diaryTitle}>오사카 마지막 날!</Text>
+        <View style={styles.bottomArea}>
+          <View style={styles.statsArea}>
+            <View style={styles.statsArea}>
+              <View style={styles.statsContainer}>
+                <TouchableOpacity onPress={() => setHeartSelected(!heartSelected)}>
+                  <Image source={heartSelected ? HeartIconFilled : HeartIcon} style={styles.icon} />
+                </TouchableOpacity>
+                <Text style={styles.statsText1}>16</Text>
+              </View>
+              <View style={styles.statsContainer}>
+                <TouchableOpacity onPress={() => setBookmarkSelected(!bookmarkSelected)}>
+                  <Image
+                    source={bookmarkSelected ? BookmarkIconFilled : BookmarkIcon}
+                    style={styles.icon}
+                  />
+                </TouchableOpacity>
+                <Text style={styles.statsText2}>32</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  diaryListBox: {
+    width: 162,
+    height: 200,
+    marginRight: 8,
+  },
+  imageExample: {
+    width: 162,
+    height: 130,
+    borderTopRightRadius: 8,
+    borderTopLeftRadius: 8,
+  },
+  infoContainer: {
+    width: 161,
+    height: 70,
+    justifyContent: 'space-between', // 세로로 요소들 사이에 공간을 분배
+    alignItems: 'flex-start', // 요소들을 위로 정렬
+    padding: 8,
+    borderWidth: 1,
+    borderTopWidth: 0, // 상단 테두리 제거
+    borderBottomRightRadius: 8,
+    borderBottomLeftRadius: 8,
+    borderColor: '#EDF2FF',
+  },
+  diaryTitle: {
+    fontSize: 16,
+    fontWeight: '400',
+    letterSpacing: -0.32,
+  },
+  bottomArea: {
+    flex: 1,
+    justifyContent: 'flex-end', // 요소들을 아래로 정렬
+    width: '100%', // 너비를 100%로 설정
+  },
+  statsArea: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end', // 아이템들을 오른쪽으로 정렬
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    marginLeft: 8,
+    gap: 2,
+    alignItems: 'center',
+  },
+  icon: {
+    width: 16,
+    height: 16,
+    marginRight: 5,
+  },
+  statsText1: {
+    color: '#FF5E5E',
+    fontSize: 14,
+    letterSpacing: -0.28,
+  },
+  statsText2: {
+    color: '#0041CB',
+    fontSize: 14,
+    letterSpacing: -0.28,
+  },
+});
+
+export default DiaryList;
