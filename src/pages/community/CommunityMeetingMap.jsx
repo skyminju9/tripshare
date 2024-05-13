1a950b7af902d607f3b7059279c317a67216d652
RetrieverOH <hyuk9833@naver.com>
2024-05-13 23:08:24 +0900

    Fix: 중복코드 제거

--- src/pages/community/CommunityMeetingMap.jsx
+++ src/pages/community/CommunityMeetingMap.jsx
@@ -32,12 +32,8 @@ import fontStyles from '../../styles/fontStyles';
 import { hourList, minuteList } from '../../dateData';
 import DatePicker from 'react-native-date-picker';
 import { APP_WIDTH } from '../../constants';
-<<<<<<< HEAD
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { formatDayTime, setAgoDays } from '../../utils/date';
-=======
-import { formatTime, setAgoDays } from '../../utils/date';
->>>>>>> b97f2c80d0616442b35a2d89b5d59d8015bd3826
 import { convertLocationToAddress } from '../../utils/convertLocation';
 import { useGeolocation } from '../../contexts/GeolocationContext';
 import { Shadow } from 'react-native-shadow-2';
